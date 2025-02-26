a7c856ab68dbf58a7e7b27f746140a8a9a0777dc
ACS27 <saundersa27@mymacewan.ca>
2025-02-25 19:14:28 -0700

    Levels
    I think this worked?
    I also changed the gravity and player speed for easier platforming, and added a second script incase we want the other character to have mirrored movements

---
+++ Scenes/camera_2d.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	 # Replace with function body.
+	Vector2(1,1)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	Vector2(1,1)
+	
